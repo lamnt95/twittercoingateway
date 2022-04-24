540040637fd4a639d5f6ee24aa14fb78a1828b15
lamnt95 <tunglam20132208@gmail.com>
2022-04-24 23:00:12 +0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <p>hello</p>
+      <iframe src="https://twitter.com/BastionProtocol/status/1517169394069098497"></iframe>
     </div>
   );
 }
